4ccf8bf151fb286e8fd778a9c105595ff200bf8c
Rabi KC <rabikc40@gmail.com>
2022-12-09 14:05:02 +0545

    Updates

--- assets/index.760dbaea.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.1c9d338f.js";const e=()=>r("div",{children:"SingleProduct"});export{e as default};
---
+++ assets/index.ad4aefa9.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.3897235e.js";const e=()=>r("div",{children:"SingleProduct"});export{e as default};
